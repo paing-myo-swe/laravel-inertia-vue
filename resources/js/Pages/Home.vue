97c920fd0229cf6c78458722b207426963749eb6
pms007 <pymyoswe007@gmail.com>
2023-12-22 15:19:49 +0700

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class="text-3xl font-bold">
+            Home
+        </h1>
+        <Nav></Nav>
+    </div>
+</template>
+
+<script setup>
+import Nav from '@/Shared/Nav.vue';
+</script>
---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <ul>
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/users">Users</Link></li>
+            <li><Link href="/settings">Settings</Link></li>
+        </ul>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
